70811bebfb6dab20c40ebfd3310efc0b32ba67c0
tangzheng <hytangzheng@126.com>
2020-09-13 16:08:38 +0800

    reset rest file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { Button } from "element-ui"
+
 Vue.config.productionTip = false
+
+Vue.use(Button)
 
 new Vue({
   router,
